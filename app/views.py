4474c0ada6129df8759216e6223fd2beba5df31d
Matt Smith <msmith@apaxsoftware.com>
2016-09-10 13:59:45 -0400

    Basic working lorem ipsum generator.

--- app/views.py
+++ app/views.py
@@ -1,3 +1,5 @@
+from os import path
+from random import choice, randint
 from flask import render_template
 
 from app import app
@@ -5,10 +7,21 @@ from app import app
 @app.route("/")
 @app.route("/index")
 def index():
-    user = {
-        "full_name": "Matt Smith",
-        "username": "mattsmith1",
-    }
+    """
+    Opens a file with lorem ipsum words in it.
+    Creates a string of lorem ipsum by pulling
+    radom words from the input file.
 
+    Displays the string of lorem ipsum to the user
+    """
+    lorem_filepath = path.join(path.dirname(__file__), "data/lorem.txt")
 
-    return render_template("index.html", user=user)
+    with open(lorem_filepath, 'r') as lorem_file:
+        all_lorem_text = lorem_file.read()
+
+    words = all_lorem_text.split(" ")
+    num_words = randint(50, 200)
+    selected_words = [choice(words) for x in range(num_words)]
+    lorem_text = " ".join(selected_words)
+
+    return render_template("index.html", lorem_text=lorem_text)
