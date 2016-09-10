0082d2e80f3277a07829afd247580b92ae1b233b
Matt Smith <msmith@apaxsoftware.com>
2016-09-10 16:00:08 -0400

    Refactor lorem generation to add cap process.

--- app/views.py
+++ app/views.py
@@ -1,8 +1,8 @@
 from os import path
-from random import choice, randint
 from flask import render_template
 
 from app import app
+from app.lib.lorem import Lorem
 
 @app.route("/")
 @app.route("/index")
@@ -14,14 +14,9 @@ def index():
 
     Displays the string of lorem ipsum to the user
     """
-    lorem_filepath = path.join(path.dirname(__file__), "data/lorem.txt")
 
-    with open(lorem_filepath, 'r') as lorem_file:
-        all_lorem_text = lorem_file.read()
-
-    words = all_lorem_text.split(" ")
-    num_words = randint(50, 200)
-    selected_words = [choice(words) for x in range(num_words)]
-    lorem_text = " ".join(selected_words)
+    filepath = path.join(path.dirname(__file__), "data/lorem.txt")
+    lorem = Lorem(words_file=filepath)
+    lorem_text = lorem.get_lorem_text(200, 500)
 
     return render_template("index.html", lorem_text=lorem_text)
